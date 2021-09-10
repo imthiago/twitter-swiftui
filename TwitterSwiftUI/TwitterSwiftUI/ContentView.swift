4f4084ce7418a4e9f0f565a4b8b236033ebd4fdf
Thiago Oliveira <thiago@outlook.com.br>
2021-09-10 20:58:10 -0300

    Adding navigation and tabbar view

--- TwitterSwiftUI/TwitterSwiftUI/ContentView.swift
+++ TwitterSwiftUI/TwitterSwiftUI/ContentView.swift
@@ -9,8 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            TabView {
+                Text("Feed")
+                    .tabItem {
+                        Image(systemName: "house")
+                        Text("Home")
+                    }
+
+                Text("Search View")
+                    .tabItem {
+                        Image(systemName: "magnifyingglass")
+                        Text("Search")
+                    }
+                Text("Message")
+                    .tabItem {
+                        Image(systemName: "envelope")
+                        Text("Messages")
+                    }
+            }
+        }
     }
 }
 
