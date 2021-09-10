a4aa8d9ac82d97e48517c85f73e025095d0a7335
Thiago Oliveira <thiago@outlook.com.br>
2021-09-10 20:23:13 -0300

    Initial project structure

---
+++ TwitterSwiftUI/TwitterSwiftUI/TwitterSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUIApp.swift
+//  TwitterSwiftUI
+//
+//  Created by Thiago Oliveira on 10/09/21.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
