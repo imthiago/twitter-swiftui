51d1400bba1a9612cfe8f60d4d8a0797546b1272
Thiago Oliveira <thiago@outlook.com.br>
2021-09-10 21:14:28 -0300

    Creating a floating action button

---
+++ TwitterSwiftUI/TwitterSwiftUI/View/FeedView.swift
@@ -0,0 +1,35 @@
+//
+//  FeedView.swift
+//  TwitterSwiftUI
+//
+//  Created by Thiago Oliveira on 10/09/21.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        ZStack(alignment: .bottomTrailing) {
+            ScrollView {
+
+            }
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Image("tweet")
+                    .resizable()
+                    .renderingMode(.template)
+                    .frame(width: 32, height: 32)
+                    .padding()
+            })
+            .background(Color(.systemBlue))
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .padding()
+        }
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
